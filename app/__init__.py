b67b56246a7df769ee3482f3c05e08ca5d04ae0c
Emman Mlmb <emmanjibi@gmail.com>
2025-06-29 01:26:54 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,19 @@ def create_app():
     app.config['SECRET_KEY'] = 'your-secret-key-123'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
+    # Initialisation des extensions
     db.init_app(app)
     bootstrap.init_app(app)
     
+    # Import et enregistrement des blueprints
     from app.routes import main_routes
     app.register_blueprint(main_routes)
     
+    # Création des tables
+    with app.app_context():
+        db.create_all()
+    
     return app
 
-# Création de l'application pour Gunicorn
+# Instance de l'application pour Gunicorn
 app = create_app()
